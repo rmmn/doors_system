0f3b006676f4885e93a799775f06b7b0aa2424f0
rmmn <43585084+rmmn@users.noreply.github.com>
2020-06-12 23:18:35 +0300

    Окно поиска

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,56 @@
 (function () {
     let menuButton = document.querySelector(".menu"),
-        menu = document.querySelector(".nav.left");
+        menu = document.querySelector(".nav.left"),
+        searchButton = document.querySelector(".nav_link.search_link"),
+        search = document.querySelector(".search"),
+        searchClose = document.querySelector(".search>.search_container>.close_search");
 
     menuButton.addEventListener("click", function (e) {
         e.preventDefault();
         menu.classList.toggle("show");
         e.currentTarget.classList.toggle("active");
-    })
+    });
+
+    searchButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        ToggleSearch(0, 300);
+    });
+
+    searchClose.addEventListener("click", function (e) {
+        e.preventDefault();
+        ToggleSearch(0, 300);
+    });
+
+    function ToggleSearch(delay = 0, duration = 300) {
+        if (!search.classList.contains("active")) {
+
+            search.style.display = "block";
+            search.animate(
+                [
+                    { opacity: 0 },
+                    { opacity: 1 }
+                ], {
+                delay: delay,
+                duration: duration
+            }
+            ).onfinish = function () {
+                search.style.opacity = 1;
+                search.classList.add("active");
+            };
+        } else {
+            search.animate(
+                [
+                    { opacity: 1 },
+                    { opacity: 0 }
+                ], {
+                delay: delay,
+                duration: duration
+            }
+            ).onfinish = function () {
+                search.style.opacity = 0;
+                search.style.display = "none";
+                search.classList.remove("active");
+            };
+        }
+    }
 })();
